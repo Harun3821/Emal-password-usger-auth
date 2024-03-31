deab1df9a3bfff4443f9611c85daf0ffbcfefc3d
Harun3821 <harun4382183@gmail.com>
2024-04-01 03:07:05 +0600

    first commit register user

--- src/Firebese/frisebess.confif.js
+++ src/Firebese/frisebess.confif.js
@@ -1,9 +1,7 @@
 
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,11 +12,10 @@ const firebaseConfig = {
   projectId: "usher-email-password",
   storageBucket: "usher-email-password.appspot.com",
   messagingSenderId: "1089072079480",
-  appId: "1:1089072079480:web:f58cb3553edb230a1e438d"
+  appId: "1:1089072079480:web:8eff100dacfe5eac1e438d"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-const auth = getAuth(app)
+const auth = getAuth(app);
 export default auth;
--- src/Pagess/Regster.jsx
+++ src/Pagess/Regster.jsx
@@ -1,28 +1,61 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../Firebese/frisebess.confif";
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
 
 
 
 
 const Regster = () => {
 
+      const [registererror, setRegistererror] = useState('');
+      
+      const [successfull, setsuccessfull] = useState('');
+
+      const [showpassword, setShowpassword] = useState(false);
+
+
      const handelRegister = (e) =>{
         e.preventDefault();
        const email = e.target.email.value;
         const password = e.target.password.value;
-        console.log(email, password)
-        
-        
+        const accept = e.target.terms.checked;
+        console.log(email, password, accept)
+     
+
+
+       setRegistererror('');
+       setsuccessfull('');
+     
+       
+
+        if(password.length < 6){
+          setRegistererror('password should be must 6 characters')
+          return;
+        }
+        else if(!/[A-Z]/.test(password)){
+            setRegistererror('password should be must uppercase characters')
+            return;
+        }
+        else if(!accept){
+          setRegistererror('Please accept our terms or condition')
+          return;
+        }
+
+
         createUserWithEmailAndPassword(auth, email, password)
         .then(result =>{
-          console.log(result.user)
-
+          const user = result.user
+          console.log(user)
+          setsuccessfull('successful registration!')
         })
-        .catch(error =>{
-            console.log(error)
+        .catch(error => {
+          console.log(error);
+          setRegistererror(error.message);
         })
-
-
+         
+      
        
 
      }
@@ -36,14 +69,36 @@ const Regster = () => {
     
        <form  onSubmit={handelRegister} className="py-10 space-y-6 text-center">
          {/* xs */}
-         <input type="email" placeholder="Email" name="email" className="input input-bordered input-md w-full max-w-xs"/>
+         <input type="email" placeholder="Email" required name="email" className="input input-bordered input-md w-full max-w-xs"/>
         {/* sm */} <br></br>
-        <input type="password" placeholder="password" name="password" className="input input-bordered input-md w-full max-w-xs" />
+          
+          <div className="relative mb-4">
+          <input type={ showpassword ? "text" : "password"}
+          placeholder="password" 
+          required name="password" 
+          className="input input-bordered input-md w-full max-w-xs"/>
+          <span className="absolute top-4 -ml-6" onClick={()=> setShowpassword(!showpassword)}>
+            {
+              showpassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
+            }
+         </span>
+         </div>
+        <br></br>
+        <div className="mb-2">
+          <input type="checkbox" name="terms" id="terms" />
+          <label className="ml-2 text-sm" htmlFor="terms">Accept our <a href="">terms and conditions</a></label>
+        </div>
+
         {/* md */}<br></br>
         <button className="btn bg-primary text-white text-3xl w-full max-w-xs">Register</button>
        </form>
+         {
+           registererror && <p className="text-2xl text-red-600 text-center">{registererror}</p>
+         }
+         {
+             successfull  && <p className="text-3xl text-green-600 text-center">{successfull}</p>
+         } 
          
-
 
         </div>
     );
